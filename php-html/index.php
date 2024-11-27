46be20cae22207cf7c92acbefba5eea86228eb5b
sitirohanah <if23.sitirohanah@mhs.ubpkarawang.ac.id>
2024-11-27 12:43:29 +0700

    Menambahkan filr pertemuan-4 ke branch pertemuan-4

---
+++ php-html/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Halaman Home</title>
+</head>
+<body>
+    <h1>Ini Halaman login</h1>
+    <from action= "login.php" method="POST">
+        <input type="./backend email" name="email" id="email" placeholder="Email"/>
+        <input type="password" name="password" id="password" placeholder="password"/>
+        <button type="submit">login</button>
+    </from>
+
+    
+</body>
+</html>
